6c4dc9a1a48aaebda40d7abafe0debf69fb4d7e8
jonghyeon <cjddnjfql@naver.com>
2023-06-08 23:13:39 +0900

    refactor: 상세페이지 컴포넌트 분리

---
+++ pages/[name].tsx
@@ -0,0 +1,42 @@
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
+import type { Store } from '../types/store';
+import DetailHeader from '@/components/home/DetailHeader';
+import DetailContent from '@/components/home/DetailContent';
+import styles from '../styles/detail.module.scss';
+
+interface Props {
+  store: Store;
+}
+
+const StoreDetail: NextPage<Props> = ({ store }) => {
+  const expanded = true;
+
+  return (
+    <div
+      className={`${styles.detailSection} ${styles.expanded} ${styles.selected}`}
+    >
+      <DetailHeader
+        currentStore={store}
+        expanded={expanded}
+        onClickArrow={() => null}
+      />
+      <DetailContent currentStore={store} expanded={expanded} />
+    </div>
+  );
+};
+export default StoreDetail;
+
+/** https://nextjs.org/docs/basic-features/data-fetching/get-static-paths */
+export const getStaticPaths: GetStaticPaths = async () => {
+  const stores = (await import('../public/stores.json')).default;
+  const paths = stores.map((store) => ({ params: { name: store.name } }));
+
+  return { paths, fallback: false };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const stores = (await import('../public/stores.json')).default;
+  const store = stores.find((store) => store.name === params?.name);
+
+  return { props: { store } };
+};
